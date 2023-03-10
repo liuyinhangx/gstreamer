78c8362b7667bf69a79f3fd993e4a71739297cf8
Yinhang Liu <yinhang.liu@intel.com>
2023-03-10 16:32:03 +0800

    msdkdec: Add function to dynamically create sink caps and src caps
    We need to create the sink caps and src caps dynamically for different
    platforms. By default, the dec init function create static pad template
    and the compatibility and flexibility of the platform are too poor.

--- subprojects/gst-plugins-bad/sys/msdk/gstmsdkcaps.h
+++ subprojects/gst-plugins-bad/sys/msdk/gstmsdkcaps.h
@@ -47,12 +47,27 @@ gst_msdkcaps_enc_create_caps (MsdkSession * session,
     GstCaps ** sink_caps, GstCaps ** src_caps);
 
 gboolean
+gst_msdkcaps_dec_create_caps (MsdkSession * session,
+    gpointer dec_description, guint codec_id,
+    GstCaps ** sink_caps, GstCaps ** src_caps);
+
+gboolean
 gst_msdkcaps_enc_append_formats (MsdkSession * session,
     GstCaps * caps, guint codec_id,
     const gchar * features, const gchar * fmts_str, gboolean need_check);
 
 gboolean
+gst_msdkcaps_dec_append_formats (MsdkSession * session,
+    GstCaps * caps, guint codec_id,
+    const gchar * features, const gchar * fmts_str, gboolean need_check);
+
+gboolean
 gst_msdkcaps_enc_set_formats (MsdkSession * session,
+    GstCaps * caps, guint codec_id,
+    const gchar * features, const gchar * fmts_str, gboolean need_check);
+
+gboolean
+gst_msdkcaps_dec_set_formats (MsdkSession * session,
     GstCaps * caps, guint codec_id,
     const gchar * features, const gchar * fmts_str, gboolean need_check);
 
